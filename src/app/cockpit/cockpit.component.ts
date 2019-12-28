4e93203706b353c950b6d4e90107a55b97a707a0
RV <razvanvancea94@gmail.com>
2019-12-28 12:34:32 +0200

    binded custom events

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: "app-cockpit",
@@ -6,6 +6,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./cockpit.component.css"]
 })
 export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+
+  @Output() bluePrintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
   newServerName = "";
   newServerContent = "";
 
@@ -14,18 +23,16 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {}
 
   onAddServer() {
-    // this.serverElements.push({
-    // type: "server",
-    // name: this.newServerName,
-    // content: this.newServerContent
-    // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
   onAddBlueprint() {
-    // this.serverElements.push({
-    // type: "blueprint",
-    // name: this.newServerName,
-    // content: this.newServerContent
-    // });
+    this.bluePrintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 }
