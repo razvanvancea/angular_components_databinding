4ebc2cb123ec3edd815c8131d4051e3f4ec9d06c
RV <razvanvancea94@gmail.com>
2019-12-28 11:47:15 +0200

    fixed protractor test

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,11 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element } from "protractor";
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get("/");
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getServernameText() {
+    return element(by.xpath('//label[text()="Server Name"]')).getText();
   }
 }
